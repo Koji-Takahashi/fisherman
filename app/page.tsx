9f62631e921e6ef1bf31d8c570854fa056aec1bf
Koji.Takahashi <ktakahashi@toyo-group.co.jp>
2025-04-14 11:47:10 +0900

    First Commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import { Authenticator } from "@aws-amplify/ui-react"; // 追加
 
 Amplify.configure(outputs);
 
@@ -33,6 +34,10 @@ export default function App() {
 
   return (
     <main>
+      <Authenticator.Provider>
+        <Authenticator>
+        </Authenticator>
+      </Authenticator.Provider>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
